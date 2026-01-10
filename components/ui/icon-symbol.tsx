120fc5dfd0b3068a05e73e17af810ddc9bd3784a
jeronimo-mg <jgomes@byupathway.edu>
2026-01-10 13:25:46 -0300

    feat: v1.0.3, fix study icon, config ios simulator

--- components/ui/icon-symbol.tsx
+++ components/ui/icon-symbol.tsx
@@ -19,6 +19,7 @@ const MAPPING = {
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
   'sparkles': 'auto-awesome',
+  'book.fill': 'menu-book',
 } as IconMapping;
 
 /**
